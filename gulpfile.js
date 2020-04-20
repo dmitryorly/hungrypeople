d874623866bed5848aedb1fe261ace9fc6aca42b
Dmitry <dmitryorly611@gmail.com>
2020-04-20 17:34:47 +0300

    Create gulpfile config
    - gulp-pug
    - gulp-htmlmin
    - gulp-sass
    - gulp-autoprefixer
    - csso
    - del
    - browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const {src, dest, series, watch} = require('gulp');
+const pug = require('gulp-pug');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const sync = require('browser-sync').create();
+
+function buildHtml() {
+  return src('src/pug/**.pug')
+    .pipe(pug({pretty: '\t'}))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(dest('dist'))
+}
+
+function buildSass() {
+  return src('src/sass/main.sass')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+  }))
+    .pipe(csso())
+    .pipe(dest('dist'))
+}
+
+function clear() {
+  return del('dist')
+}
+
+function serve() {
+  sync.init({
+    server: './dist'
+  })
+
+  watch('src/pug/**.pug', series(buildHtml)).on('change', sync.reload)
+  watch('src/sass/**.sass', series(buildSass)).on('change', sync.reload)
+}
+
+exports.build = series(clear, buildHtml, buildSass)
+exports.clear = clear;
+exports.serve = series(clear, buildHtml, buildSass, serve);
