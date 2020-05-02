1e932422c210d367ea827f5fb6c09819fb2dabd8
Dmitry <dmitryorly611@gmail.com>
2020-05-02 19:16:46 +0300

    Connect main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
-function fun(){
+// UNWRAP BUTTON
 
-}
+(function(){
+  let buttons = Array.from(document.querySelectorAll('.js-unwrap'));
+  buttons.forEach( button => {
+    button.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      console.log('clicked')
+    })
+  })
+
+}());
