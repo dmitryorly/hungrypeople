ac15302c23a6750f7f2d81efc3e502f0b80e9f82
Dmitry <dmitryorly611@gmail.com>
2020-05-03 00:20:18 +0300

    Useless handlers for accordeons

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 // UNWRAP BUTTON
 
-(function(){
-  let buttons = Array.from(document.querySelectorAll('.js-unwrap'));
-  buttons.forEach( button => {
-    button.addEventListener('click', (ev) => {
-      ev.preventDefault();
-      console.log('clicked')
-    })
-  })
-
-}());
+// (function(){
+  // let buttons = Array.from(document.querySelectorAll('.js-unwrap'));
+  // buttons.forEach( button => {
+    // button.addEventListener('click', (ev) => {
+      // ev.preventDefault();
+      // console.log('clicked')
+    // })
+  // })
+// 
+// }());
