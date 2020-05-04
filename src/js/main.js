6bdc6a3c18cd32550fe268e80bdb64988810ce0e
Dmitry <dmitryorly611@gmail.com>
2020-05-04 23:25:38 +0300

    Mobile menu without animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,29 @@
-// UNWRAP BUTTON
+// MOBILE MENU
 
-// (function(){
-  // let buttons = Array.from(document.querySelectorAll('.js-unwrap'));
-  // buttons.forEach( button => {
-    // button.addEventListener('click', (ev) => {
-      // ev.preventDefault();
-      // console.log('clicked')
-    // })
-  // })
-// 
-// }());
+(function(){
+
+  let body = document.querySelector('body'),
+    menu = body.querySelector('.js-menu'),
+    menuBtn = body.querySelector('.js-menuBtn'),
+    links = Array.from(menu.querySelectorAll('.header__link'));
+
+  // Menu close - open handler
+  menuBtn.addEventListener('click', () => {
+    body.classList.toggle('overflow_hidden');
+    menu.classList.toggle('mobile_show');
+    menuBtn.classList.toggle('mobile__btn_close'); // NEED ANIMATION
+  } )
+
+  // Links click handler
+  let linkClickHandler = function() {
+    body.classList.remove('overflow_hidden');
+    menu.classList.remove('mobile_show');
+    menuBtn.classList.remove('mobile__btn_close'); // NEED ANIMATION
+  }
+
+  // Links listeners
+  links.forEach( link => {
+    link.addEventListener('click', linkClickHandler)
+  })
+
+}());
