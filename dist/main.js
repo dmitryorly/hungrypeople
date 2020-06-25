4b6cd5c8ea3b215c73ca860bd28f23cc96180cb6
Dmitry <dmitryorly611@gmail.com>
2020-06-25 16:52:40 +0300

    build v0.2.2

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,34 @@
-// UNWRAP BUTTON
+// MOBILE MENU
 
 (function(){
-  let buttons = Array.from(document.querySelectorAll('.js-unwrap'));
-  buttons.forEach( button => {
-    button.addEventListener('click', (ev) => {
-      // ev.preventDefault();
-      console.log('clicked')
-    })
+
+  let body = document.querySelector('body'),
+    menu = body.querySelector('.js-menu'),
+    menuBtn = body.querySelector('.js-menuBtn'),
+    menuLines = Array.from(body.querySelectorAll('.mobile__line')),
+    links = Array.from(menu.querySelectorAll('.header__link'));
+
+  // Menu close - open handler
+  menuBtn.addEventListener('click', () => {
+    body.classList.toggle('overflow_hidden');
+    menu.classList.toggle('mobile_show');
+    // menuBtn.classList.toggle('mobile__btn_close'); // NEED ANIMATION
+    // console.log(menuLines)
+    
+    //Animations
+    menuLines.forEach( line => line.classList.toggle('mobile__line_active') );
+  } )
+
+  // Links click handler
+  let linkClickHandler = function() {
+    body.classList.remove('overflow_hidden');
+    menu.classList.remove('mobile_show');
+    // menuBtn.classList.remove('mobile__btn_close'); // NEED ANIMATION
+  }
+
+  // Links listeners
+  links.forEach( link => {
+    link.addEventListener('click', linkClickHandler)
   })
 
 }());
